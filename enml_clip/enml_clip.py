a9fb3c4d4cc09042092d03fac338a0757b51eb87
Administrator <zhanlongbo@qq.com>
2021-12-03 16:10:56 +0800

    印象笔记复制粘贴小工具

--- enml_clip/enml_clip.py
+++ enml_clip/enml_clip.py
@@ -3,20 +3,33 @@ import keyboard
 import sys
 
 def quit():
+    print("exit..")
     sys.exit(0);
 
 def enml_clip():
     dd = pyperclip.paste()
-    res = ""
+    line_res = []
     lines = dd.split("\n")
     for line in lines:
         line = line
         if not line.strip():
             continue
-        if line.endswith(" \r"):
-            res += line[0:-2]
+        #print("row:"+line)
+        if line.endswith(". \r"):
+            line_res.append(line[0:-2])
         else:
-            res += line + "\n"
+            line_res.append(line + "\n")
+    if len(line_res) == 0:
+        return
+
+    start_empty_count = 0
+    while line_res[0][start_empty_count] == ' ' or line_res[0][start_empty_count] == '\t':
+        start_empty_count += 1
+    for i in range(len(line_res)):
+        line_res[i] = line_res[i][start_empty_count:]
+        #print(line_res[i])
+
+    res = "".join(line_res)
     print(res)
     pyperclip.copy(res)
 
