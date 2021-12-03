bf9ac08e3e6ce26c265da88e27ae984f8a804b6b
Administrator <zhanlongbo@qq.com>
2021-12-03 17:05:27 +0800

    印象笔记复制粘贴小工具

--- enml_clip/enml_clip.py
+++ enml_clip/enml_clip.py
@@ -2,34 +2,38 @@ import pyperclip
 import keyboard
 import sys
 
+
 def quit():
     print("exit..")
     sys.exit(0);
 
+
 def enml_clip():
     dd = pyperclip.paste()
-    line_res = []
     lines = dd.split("\n")
+    first = True
+    first_emtpy_count = 0
+    res = ""
     for line in lines:
-        line = line
         if not line.strip():
             continue
-        #print("row:"+line)
-        if line.endswith(". \r"):
-            line_res.append(line[0:-2])
+        print("row:" + line)
+        # line_res.append(line)
+        is_num_list = True
+        if line.endswith(". \r"):  # 列表数字后面被直接换行了
+            line = line[0:-1]
         else:
-            line_res.append(line + "\n")
-    if len(line_res) == 0:
-        return
+            line = line + "\n"  # 列表的实际内容
+            is_num_list = False
 
-    start_empty_count = 0
-    while line_res[0][start_empty_count] == ' ' or line_res[0][start_empty_count] == '\t':
-        start_empty_count += 1
-    for i in range(len(line_res)):
-        line_res[i] = line_res[i][start_empty_count:]
-        #print(line_res[i])
+        if first:
+            while line[first_emtpy_count] == '\t':
+                first_emtpy_count += 1
+        if is_num_list or first:
+            line = line[first_emtpy_count:]
+        res += line
+        first = False
 
-    res = "".join(line_res)
     print(res)
     pyperclip.copy(res)
 
